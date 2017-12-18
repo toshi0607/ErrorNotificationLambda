1fc2ada6f3b207145a4aa2d454086855396c811c
toshi0607 <s.toshi0607@gmail.com>
2017-12-18 23:04:06 +0900

    データのデコード

--- ErrorNotificationLambda.Tests/FunctionTest.cs
+++ ErrorNotificationLambda.Tests/FunctionTest.cs
@@ -30,7 +30,7 @@ namespace ErrorNotificationLambda.Tests
 	        {
 		        Awslogs = new LogEvent.Log
 				{
-			        Data = "error!" + Environment.NewLine + "stackTrace at hogehogehogehoge"
+                    Data = "H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA=="
 				}
 	        };
 
