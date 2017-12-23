bad9391ad35b8fcd6933d1ec32cf142ed0ee4a9c
toshi0607 <s.toshi0607@gmail.com>
2017-12-23 21:50:19 +0900

    フォーマット

--- ErrorNotificationLambda.Tests/FunctionTest.cs
+++ ErrorNotificationLambda.Tests/FunctionTest.cs
@@ -6,26 +6,26 @@ namespace ErrorNotificationLambda.Tests
 {
     public class FunctionTest : IClassFixture<LaunchSettingsFixture>
     {
-	    LaunchSettingsFixture _fixture;
+        LaunchSettingsFixture _fixture;
 
         public FunctionTest(LaunchSettingsFixture fixture)
         {
             _fixture = fixture;
         }
 
-		[Fact]
+        [Fact]
         public async void TestErrorNotificationFunction()
         {
             var function = new Function();
             var context = new TestLambdaContext();
 
-	        var evnt = new CloudWatchLogsEvent()
-	        {
-		        Awslogs = new CloudWatchLogsEvent.Log
+            var evnt = new CloudWatchLogsEvent()
+            {
+                Awslogs = new CloudWatchLogsEvent.Log
                 {
                     EncodedData = "H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA=="
                 }
-	        };
+            };
 
             var notification = await function.FunctionHandler(evnt, context);
 
--- ErrorNotificationLambda/Function.cs
+++ ErrorNotificationLambda/Function.cs
@@ -17,61 +17,59 @@ namespace ErrorNotificationLambda
 {
     public class Function
     {
-
-		/// <summary>
+        /// <summary>
         /// Notify contents waritten out on CloudWatch Logs
-		/// </summary>
-		/// <param name="logEvent"></param>
-		/// <param name="context"></param>
-		/// <returns></returns>
-		public async Task<bool> FunctionHandler(CloudWatchLogsEvent logEvent, ILambdaContext context)
-		{
-			var slackWebhookUrl = Environment.GetEnvironmentVariable("SLACK_WEBHOOK_URL");
-			var cloudWatchLogGroupUrl = Environment.GetEnvironmentVariable("CLOUDWATCH_LOG_GROUP_URL");
-			var cloudWatchMetricsUrl = Environment.GetEnvironmentVariable("CLOUDWATCH_METRICS_URL");
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<bool> FunctionHandler(CloudWatchLogsEvent logEvent, ILambdaContext context)
+        {
+            var slackWebhookUrl = Environment.GetEnvironmentVariable("SLACK_WEBHOOK_URL");
+            var cloudWatchLogGroupUrl = Environment.GetEnvironmentVariable("CLOUDWATCH_LOG_GROUP_URL");
+            var cloudWatchMetricsUrl = Environment.GetEnvironmentVariable("CLOUDWATCH_METRICS_URL");
 
-			var payload = new
-			{
-				channel = "dev",
-				username = "CloudWatch Notification",
-                text = $"{GetMessage(logEvent.Awslogs.DecodeData())}"+ Environment.NewLine +
+            var payload = new
+            {
+                channel = "dev",
+                username = "CloudWatch Notification",
+                text = $"{GetMessage(logEvent.Awslogs.DecodeData())}" + Environment.NewLine +
                        $"Logs: <{cloudWatchLogGroupUrl}|Click here>" + Environment.NewLine +
                        $"Metrics: <{cloudWatchMetricsUrl}|Click here>",
-			};
+            };
 
-			var jsonString = JsonConvert.SerializeObject(payload);
+            var jsonString = JsonConvert.SerializeObject(payload);
 
-			var content = new FormUrlEncodedContent(new Dictionary<string, string>
-			{
-				{ "payload", jsonString}
-			});
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "payload", jsonString}
+            });
 
-			try
-			{
-				using (var client = new HttpClient())
-				{
-					await client.PostAsync(slackWebhookUrl, content);
-				}
-			}
-			catch (Exception e)
-			{
-				context.Logger.LogLine("error!!!!" + Environment.NewLine + $"{e.Message}" + Environment.NewLine + $"{e.StackTrace}");
-				throw;
-			}
-			
-			return true;
-		}
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    await client.PostAsync(slackWebhookUrl, content);
+                }
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine("error!!!!" + Environment.NewLine + $"{e.Message}" + Environment.NewLine + $"{e.StackTrace}");
+                throw;
+            }
 
-	    private static string GetMessage(string decodedData)
-	    {
-		    string extractedMessage = "";
-		    var messages = JObject.Parse(decodedData).GetValue("logEvents");
-		    foreach (var item in messages)
-		    {
-			    extractedMessage += item["message"].Value<string>() + Environment.NewLine;
-		    }
+            return true;
+        }
+
+        private static string GetMessage(string decodedData)
+        {
+            string extractedMessage = "";
+            var messages = JObject.Parse(decodedData).GetValue("logEvents");
+            foreach (var item in messages)
+            {
+                extractedMessage += item["message"].Value<string>() + Environment.NewLine;
+            }
             return extractedMessage;
-	    }
-
+        }
     }
 }
